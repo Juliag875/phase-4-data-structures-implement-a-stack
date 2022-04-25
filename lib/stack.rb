02fc3551ed513490171e9d6f1ac16a37501c5f7f
Iuliia El Sammak <juliag875@gmail.com>
2022-04-24 20:47:57 -0400

    commit tree 474a72451e4573a99634ae3fe8869c1d4b920c70

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,41 @@
-# your code here
+class Stack
+
+  def initialize(limit = nil)
+    @stack = []
+    @limit = limit
+  end
+
+  def push(value)
+    raise "Stack Overflow" if full?
+    @stack.push(value)
+  end
+
+  def pop 
+    @stack.pop
+  end
+  
+  def peek
+    @stack.last
+  end
+
+  def size
+    @stack.length
+  end
+
+  def empty?
+    @stack.empty?
+  end
+
+  def full?
+    @limit && @stack.size == @limit
+  end
+
+  def search(target)
+    @stack.each_with_index do |i, index|
+      return size - index - 1 if i == target
+    end
+    -1
+  end
+
+
+end
